377012b74d314baca1db1fcd6eb37e6af5a3f1ea
Kadir Beşkardeş <kadir.beskardes11@gmail.com>
2025-12-17 16:15:21 +0300

    Refactor models, remove localization, update .gitignore
    - Split ProjectListing, ProjectDetail, and ContactModel into separate files under Personal.Models
    - Remove all .NET resource-based localization files and marker class
    - Reformat projects.tr.json for consistency (no data changes)

---
+++ Models/ProjectListing.cs
@@ -0,0 +1,13 @@
+﻿namespace Personal.Models
+{
+    public class ProjectListing
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string ImageUrl { get; set; }
+        public string[] Technologies { get; set; }
+        public string Category {  get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
--- Resources/SharedResource.cs
+++
@@ -1,8 +0,0 @@
-namespace Personal.Resources
-{
-    public class SharedResource
-    {
-        // This class is empty but it's needed for localization to work
-        // The resource files will be mapped to this class
-    }
-}
